7a6e5c83c1ee57aab56155a1e968e281c5669b2e
omar <omar@zedia-agency.com>
2026-02-18 19:43:09 +0200

    fix: finalize logo usage (header=logo, hero/footer=logoW)

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -64,7 +64,7 @@ export function SiteFooter() {
             {/* Brand */}
             <div className="space-y-4">
               <img
-                src="/images/brand/logoW.png"
+  src="/images/brand/logoW.png"
                 alt="La Cueva Aqua Park"
                 className="h-16 w-auto object-contain"
               />
